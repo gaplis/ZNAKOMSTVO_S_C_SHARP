4a47a1d2068772f3e11bb942c6e491a549b323f3
IlyaChistov <gaplis322@gmail.com>
2022-09-05 15:05:22 +0300

    Task 27 completed

---
+++ homework_seminar_4/task_27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Sum(int number)
+{
+    int result = 0;
+    while(number != 0)
+    {
+        int percent = number % 10;
+        result += percent;
+        number = number / 10;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Sum(num));
