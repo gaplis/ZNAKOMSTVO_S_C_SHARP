ccde4b6914d0b16efbbf30446e579e730da5b012
IlyaChistov <gaplis322@gmail.com>
2022-09-05 15:42:50 +0300

    Task 29 completed

---
+++ homework_seminar_4/task_29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов, заполненных случайнми числами из [a, b) и выводит их на экран.
+
+int[] InitArray(int N, int min, int max)
+{
+    int[] arr = new int[N];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(min, max);
+    }
+    return arr;
+}
+
+Console.Write("Укажите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+Console.Write("Укажите минимальное значение: ");
+int startNum = int.Parse(Console.ReadLine());
+Console.Write("Укажите максимальное значение: ");
+int endNum = int.Parse(Console.ReadLine());
+
+Console.WriteLine(String.Join(", ", InitArray(size, startNum, endNum)));
