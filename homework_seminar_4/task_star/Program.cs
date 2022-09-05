3ed7953c08e57467a05b547414e2eaa4dc2b32ec
IlyaChistov <gaplis322@gmail.com>
2022-09-05 16:37:31 +0300

    Task star completed

---
+++ homework_seminar_4/task_star/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача “со звездочкой”: Напишите функцию, которая принимает одно число - высоту елочки и рисует ее в консоли звездочками.
+
+void Spruce(int number)
+{
+    int count = 0;
+    int amountString = number;
+    string star = "*";
+    while(count < number)
+    {
+        int space = 1;
+        while(space < amountString)
+        {
+            Console.Write(" ");
+            space++;
+        }
+        Console.WriteLine(star);
+        count++;
+        amountString--;
+        star = star + "**";
+    }
+}
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+Spruce(num);
+
+//    *
+//   ***
+//  *****
+// *******
+//*********
