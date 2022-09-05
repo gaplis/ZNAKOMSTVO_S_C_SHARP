4c7bcaec7885cfed2f1a7930b4fc7e265acb2a45
IlyaChistov <gaplis322@gmail.com>
2022-09-05 14:42:27 +0300

    Task 25 completed

---
+++ homework_seminar_4/task_25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Exponentiate(int xA, int xB)
+{
+    int result = xA;
+    for (int i = 1; i < xB ; i++)
+    {
+        result = result * xA; 
+    }
+    return result;
+}
+
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Exponentiate(A, B));
