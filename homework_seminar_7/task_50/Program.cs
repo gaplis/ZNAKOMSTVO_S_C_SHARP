68dc714929f1cce74ed7f3b10c9661388965fded
IlyaChistov <gaplis322@gmail.com>
2022-09-15 15:03:26 +0300

    Task 50 completed

--- homework_seminar_7/task_50/Program.cs
+++ homework_seminar_7/task_50/Program.cs
@@ -1,13 +1,14 @@
-﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+﻿// Задача 50. Напишите программу, которая на вход принимает число и генерирует случайный двумерный массив,
+// и возвращает индексы этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
 
-// m = 3, n = 4.
-// 0,5 7 -2 -0,2
-// 1 -3,3 8 -9,9
-// 8 7,8 -7,1 9
-
-int[,] InitArray(int colString, int colColumns, int minValue, int maxValue)
+int[,] GetArray(int colString, int colColumnls, int minValue, int maxValue)
 {
-    int[,] array = new int[colString, colColumns];
+    int[,] array = new int[colString, colColumnls];
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
@@ -16,32 +17,6 @@ int[,] InitArray(int colString, int colColumns, int minValue, int maxValue)
         }
     }
     return array;
-}
-
-double[,] InitDoubleArray(int colString, int colColumns)
-{
-    double[,] array = new double[colString, colColumns];
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().NextDouble();
-        }
-    }
-    return array;
-}
-
-double[,] FullArray(int[,] array, double[,] arrayDouble)
-{
-    double[,] arrayFull = new double[array.GetLength(0), array.GetLength(1)];
-    for (int i = 0; i < arrayFull.GetLength(0); i++)
-    {
-        for (int j = 0; j < arrayFull.GetLength(1); j++)
-        {
-            arrayFull[i, j] = array[i, j] + arrayDouble[i, j];
-        }
-    }
-    return arrayFull;
 }
 
 void PrintArray(int[,] array)
@@ -56,16 +31,16 @@ void PrintArray(int[,] array)
     }
 }
 
-void PrintDoubleArray(double[,] array)
+bool FindNumber(int[,] array, int number)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write($"{array[i, j]:f2}\t ");
+            if (array[i,j] == number) return true;
         }
-        Console.WriteLine();
     }
+    return false;
 }
 
 Console.Write("Задайте количество строк массива: ");
@@ -82,18 +57,18 @@ if (sizeString > 0 && sizeColumns > 0)
     Console.WriteLine();
     if (numMax >= numMin)
     {
-        int[,] myArray = InitArray(sizeString, sizeColumns, numMin, numMax);
-        double[,] myDoubleArray = InitDoubleArray(sizeString, sizeColumns);
-        double[,] myFullArray = FullArray(myArray, myDoubleArray);
+        Console.Write("Введите искомое число: ");
+        int findNum = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        int[,] myArray = GetArray(sizeString, sizeColumns, numMin, numMax);
+        
 
         PrintArray(myArray);
         Console.WriteLine();
 
-        PrintDoubleArray(myDoubleArray);
-        Console.WriteLine();
-
-        PrintDoubleArray(myFullArray);
-        Console.WriteLine();
+        if (FindNumber(myArray, findNum)) Console.WriteLine($"Число {findNum} найдено!");
+        else Console.WriteLine($"Числа {findNum} нет в массиве.");
     }
     else Console.WriteLine("Максимальное значение числа не может быть меньше минимального.");
 }
