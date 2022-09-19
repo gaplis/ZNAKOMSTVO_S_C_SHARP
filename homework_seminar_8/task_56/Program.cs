b1ec186f4f4000799ac81f48d2bb41ce87b00c65
IlyaChistov <gaplis322@gmail.com>
2022-09-19 18:34:44 +0300

    Task 56 completed

---
+++ homework_seminar_8/task_56/Program.cs
@@ -0,0 +1,91 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2 - 14
+// 5 9 2 3 - 19
+// 8 4 2 4 - 18
+// 5 2 6 7 - 20
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] GetArray(int colString, int colColumnls, int minValue, int maxValue)
+{
+    int[,] array = new int[colString, colColumnls];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] SumString(int[,] array)
+{
+    int[] sumArray = new int[array.GetLength(0)];
+    int k = 0;
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        sumArray[k] = sum;
+        sum = 0;
+        k++;
+    }
+    return sumArray;
+}
+
+int FindMin(int[] array)
+{
+    int result = 0;
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+            result = i;
+        }
+    }
+    return result;
+}
+
+Console.Write("Задайте количество строк массива: ");
+int sizeString = int.Parse(Console.ReadLine());
+Console.Write("Задайте количество столбцов массива: ");
+int sizeColumns = int.Parse(Console.ReadLine());
+
+if (sizeString > 0 && sizeColumns > 0)
+{
+    Console.Write("Задайте минимальное значение чисел: ");
+    int numMin = int.Parse(Console.ReadLine());
+    Console.Write("Задайте максимальное значение чисел: ");
+    int numMax = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (numMax >= numMin)
+    {
+        int[,] myArray = GetArray(sizeString, sizeColumns, numMin, numMax);
+        PrintArray(myArray);
+        Console.WriteLine();
+        int[] mySumArray = SumString(myArray);
+        Console.WriteLine(String.Join(" ", mySumArray));
+        int minSum = FindMin(mySumArray);
+        Console.WriteLine($"Строка с наименьшей суммой элементов - {minSum + 1}");
+    }
+    else Console.WriteLine("Максимальное значение числа не может быть меньше минимального.");
+}
+else Console.WriteLine("Размер строк и столбцов массива не может быть нулевым или отрицательным.");
