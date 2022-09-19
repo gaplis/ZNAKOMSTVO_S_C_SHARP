596bbad134fc2d0f77d99016adcd8e627e8d2fa4
IlyaChistov <gaplis322@gmail.com>
2022-09-19 18:09:38 +0300

    Task 54 completed

---
+++ homework_seminar_8/task_54/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] GetArray(int colString, int colColumnls, int minValue, int maxValue)
+{
+    int[,] array = new int[colString, colColumnls];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SortArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, j] < array[i, k])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+Console.Write("Задайте количество строк массива: ");
+int sizeString = int.Parse(Console.ReadLine());
+Console.Write("Задайте количество столбцов массива: ");
+int sizeColumns = int.Parse(Console.ReadLine());
+
+if (sizeString > 0 && sizeColumns > 0)
+{
+    Console.Write("Задайте минимальное значение чисел: ");
+    int numMin = int.Parse(Console.ReadLine());
+    Console.Write("Задайте максимальное значение чисел: ");
+    int numMax = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (numMax >= numMin)
+    {
+        int[,] myArray = GetArray(sizeString, sizeColumns, numMin, numMax);
+        PrintArray(myArray);
+        Console.WriteLine();
+        int[,] mySortArray = SortArray(myArray);
+        PrintArray(mySortArray);
+    }
+    else Console.WriteLine("Максимальное значение числа не может быть меньше минимального.");
+}
+else Console.WriteLine("Размер строк и столбцов массива не может быть нулевым или отрицательным.");
